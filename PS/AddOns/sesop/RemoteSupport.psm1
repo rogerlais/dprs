127778a6a022ea46b0925f809c8bcf60dc3b02f2
Rogerlais Andrade e Silva <rogerlais@gmail.com>
2016-08-25 11:20:47 -0300

    set-vnstate e limpeza alterados

--- PS/AddOns/sesop/RemoteSupport.psm1
+++ PS/AddOns/sesop/RemoteSupport.psm1
@@ -12,14 +12,13 @@
    Recebe nome do computador e credenciais para ativar o serviço VNC na estação especificada
 #>
 function Set-VNCState{
-    <#
     [CmdletBinding()]
     [OutputType([int])]
     Param(
         # Descrição da ajuda de parâm1
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=0)] [string] $pc,
-        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=1, )] [boolean] $state,
-        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, Position=2)] [System.Management.Automation.PSCredential] $credential
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=0)] [string] $Computername,
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, Position=1)] [boolean] $state,
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, Position=2)] [System.Management.Automation.PSCredential] $Credential
     )
 
     Begin{
@@ -29,17 +28,14 @@ function Set-VNCState{
     }
     Process{        
         if( $state ){
-            $result = (Get-WmiObject Win32_Service -ComputerName $pc -Credential $credential -Filter {(name = "WinVNC")}).StartService()
+            $result = (Get-WmiObject Win32_Service -ComputerName $Computername -Credential $Credential -Filter {(name = "WinVNC")}).StartService()
         }else{
-            $result = (Get-WmiObject Win32_Service -ComputerName $pc -Credential $credential -Filter {(name = "WinVNC")}).StopService()
+            $result = (Get-WmiObject Win32_Service -ComputerName $Computername -Credential $Credential -Filter {(name = "WinVNC")}).StopService()
         }
-        Write-Host (" - WinVNC: {0}" -f (Get-WmiObject Win32_Service -ComputerName $pc -Credential $credential -Filter {(name = "WinVNC")} | Select-Object -ExpandProperty State))
+        Write-Host (" - WinVNC: {0}" -f (Get-WmiObject Win32_Service -ComputerName $Computername -Credential $Credential -Filter {(name = "WinVNC")} | Select-Object -ExpandProperty State))
     }
     End {
         return $result
     }
-
-    #>
 }
-
 #export-modulemember -function * -Alias *
